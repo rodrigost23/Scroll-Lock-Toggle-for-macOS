355ec7651113533a024575e67089137de1410eac
Rodrigo Tavares <rodrigo@iMac-de-Rodrigo.local>
2017-03-26 19:23:17 -0400

    Initial Commit

---
+++ kb LED Menu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  kb LED Menu
+//
+//  Created by Rodrigo Tavares on 26/03/17.
+//  Copyright © 2017 Rodrigo Tavares. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
